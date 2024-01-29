04eabfd8992bfc0f142fe4c360734bb68b778afe
George <lopatkogeorgii@gmail.com>
2024-01-29 18:22:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.birthdaycardapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birthdaycardapp.ui.theme.BirthdayCardAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BackgroundImage(name = "George", from = "Emma")
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingText(name: String, from: String, modifier: Modifier= Modifier) {
+    Column(modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Happy birthday, $name",
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "From $from",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun BackgroundImage(name: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bdaycardbackground)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = "Birthday card background",
+            contentScale = ContentScale.Crop,
+            alpha = 0.8F,
+            modifier = Modifier
+                    .fillMaxSize()
+        )
+        GreetingText(
+            name = "George",
+            from = "Emma",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+            )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    BirthdayCardAppTheme {
+        BackgroundImage(name = "123", from = "123")
+    }
+}
